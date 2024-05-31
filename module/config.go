e315a35420b47eaf1e254bc4f779bb90f653d30a
jul003 <dzulkiflifaiz11@gmail.com>
2024-05-31 15:07:31 +0700

    hahay

---
+++ module/config.go
@@ -0,0 +1,15 @@
+package _714220030
+
+import (
+	"github.com/aiteung/atdb"
+	"os"
+)
+
+var MongoString string = os.Getenv("MONGOSTRING")
+
+var MongoInfo = atdb.DBInfo{
+	DBString: MongoString,
+	DBName:   "tesdb2024",
+}
+
+var MongoConn = atdb.MongoConnect(MongoInfo)
